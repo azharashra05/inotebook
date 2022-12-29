d29b2ad6f0f3289a7f09ecbf44fb059fff6e1683
Md Azhar Ashraf <azhar.ashraf@milliman.com>
2022-12-29 19:32:21 +0530

    Creating thunder client collections to manage requests

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,17 @@
-const connectToMongo=require('./db')
+const connectToMongo = require('./db');
 const express = require('express')
 
-connectToMongo()
-
+connectToMongo();
 const app = express()
-const port = 3000
+const port = 5000
 
 app.use(express.json())
-//Available Routes
-app.use('/api/auth',require('./routes/auth'))
-app.use('/api/notes',require('./routes/notes'))
+
+// Available Routes
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
+
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening at http://localhost:${port}`)
 })
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,24 +1,38 @@
 const express = require('express');
 const User = require('../models/User');
 const router = express.Router();
-const { body, validationResult } = require('express-validator'); 
+const { body, validationResult } = require('express-validator');
 
 
-// Create a User using: POST "/api/auth/". Doesn't require Auth
-router.post('/',[body('name','Enter a valid name').isLength({ min: 3 }),body('email','Enter a valid email').isEmail(),body('password','Password must be atleast 5 length characters').isLength({ min: 5 }),], (req, res)=>{ 
-console.log(req.body);
-// Finds the validation errors in this request and wraps them in an object with handy functions
-const errors = validationResult(req);
-if (!errors.isEmpty()) {
-  return res.status(400).json({ errors: errors.array() });
-}
-User.create({
-    name: req.body.name,
-    email:req.body.email,
-    password: req.body.password,
-    }).then(user => res.json(user))
-    .catch(err=> {console.log(err)
-        res.json({error: 'Please enter a unique value for email', message: err.message})});
-} )
+// Create a User using: POST "/api/auth/createuser". No login required
+router.post('/createuser', [
+  body('name', 'Enter a valid name').isLength({ min: 3 }),
+  body('email', 'Enter a valid email').isEmail(),
+  body('password', 'Password must be atleast 5 characters').isLength({ min: 5 }),
+], async (req, res) => {
+  // If there are errors, return Bad request and the errors
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  // Check whether the user with this email exists already
+  try {
+    let user = await User.findOne({ email: req.body.email });
+    if (user) {
+      return res.status(400).json({ error: "Sorry a user with this email already exists" })
+    }
+    // Create a new user
+    user = await User.create({
+      name: req.body.name,
+      password: req.body.password,
+      email: req.body.email,
+    })
+    res.json(user)
+    
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Some Error occured");
+  }
+})
 
 module.exports = router
