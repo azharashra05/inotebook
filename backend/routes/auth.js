84d5724e968867d2c7c33e1b5aecb640ae7f97e3
Md Azhar Ashraf <azhar.ashraf@milliman.com>
2022-12-30 16:07:40 +0530

    Creating login endpoints and sending auth token

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -43,8 +43,48 @@ router.post('/createuser', [
     
   } catch (error) {
     console.error(error.message);
-    res.status(500).send("Some Error occured");
+    res.status(500).send("Internal Sever Error");
   }
 })
 
+// Authenticate a User using: POST "/api/auth/login". Login required
+router.post('/login', [
+  body('email', 'Enter a valid email').isEmail(),
+  body('password', 'Password cannot be blank').exists()
+], async (req, res) => 
+   {
+    // If there are errors, return Bad request and the errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {email,password}=req.body;
+    try{
+      let user=await User.findOne({email})
+      if(!user)
+      {
+        return res.status(400).json({error:"Please try to login with correct credentials"})
+      }
+      const passwordCompare=await bcrypt.compare(password,user.password)
+      if(!passwordCompare)
+      {
+        return res.status(400).json({error:"Please try to login with correct credentials"})
+      }
+
+      const data={
+        user:{
+          id:user.id
+        }
+      }
+      const authToken=jwt.sign(data,JWT_SECRET)
+      res.json({authToken})
+
+    }
+    catch (error) {
+      console.error(error.message);
+      res.status(500).send("Internal Sever Error");
+    }
+   }
+)
 module.exports = router
