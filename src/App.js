12111c5b687d3f13a5948ec45c84cbc069dc4ddc
Md Azhar Ashraf <azhar.ashraf@milliman.com>
2023-01-07 12:31:50 +0530

    Created Navbar and Routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import About from './components/About';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div>
-      <h1>This is iNotebook</h1>
-    </div>
+    <>
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+      </Routes>
+    </Router>
+    </>
   );
 }
 
