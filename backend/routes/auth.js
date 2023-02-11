ab551e3a39f3500587056490e104cfa93cdbf050
Md Azhar Ashraf <azhar.ashraf@milliman.com>
2023-02-11 22:35:24 +0530

    Added Signup component

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -15,16 +15,17 @@ router.post('/createuser', [
   body('email', 'Enter a valid email').isEmail(),
   body('password', 'Password must be atleast 5 characters').isLength({ min: 5 }),
 ], async (req, res) => {
+  let success=false
   // If there are errors, return Bad request and the errors
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
+    return res.status(400).json({success,errors: errors.array() });
   }
   // Check whether the user with this email exists already
   try {
     let user = await User.findOne({ email: req.body.email });
     if (user) {
-      return res.status(400).json({ error: "Sorry a user with this email already exists" })
+      return res.status(400).json({ success,error: "Sorry a user with this email already exists" })
     }
     const salt=await bcrypt.genSalt(10)
     const secPass=await bcrypt.hash(req.body.password,salt)
@@ -40,7 +41,8 @@ router.post('/createuser', [
       }
     }
     const authToken=jwt.sign(data,JWT_SECRET)
-    res.json({authToken})
+    success=true
+    res.json({success,authToken})
     
   } catch (error) {
     console.error(error.message);
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,9 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Signup = () => {
+  const [user, setUser] = useState({name:"",email:"",password:""})
+  let navigate=useNavigate()
+  const onChange=(e)=>
+  {
+    setUser({...user,[e.target.name]:e.target.value})
+  }
+  const handleSubmit=async(e)=>
+  {
+    e.preventDefault()
+    const response=await fetch(`http://localhost:5000/api/auth/createuser`,
+      {
+        method:'POST',
+        headers:
+        {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({name:user.name,email:user.email,password:user.password})
+      }
+      )
+      const json=await response.json()
+      console.log(json)
+      if(json.success)
+      {
+        //Save the auth token and redirect
+        localStorage.setItem('token',json.authToken)
+        navigate('/')
+      }
+      else
+      {
+        alert(json.error)
+      }
+  }
   return (
-    <div>
-      I am Signup
+    <div className='container'>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label htmlFor="name" className="form-label">Name</label>
+          <input type="text" className="form-control" id="name" name="name" value={user.name} onChange={onChange} minLength={3} required/>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">Email</label>
+          <input type="email" className="form-control" id="email" name="email" value={user.email} onChange={onChange} required/>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">Password</label>
+          <input type="password" className="form-control" id="password" name="password" value={user.password} onChange={onChange} minLength={5} required/>
+        </div>
+        <button disabled={user.name.length<3 || user.password.length<5} type="submit" className="btn btn-primary">Submit</button>
+      </form>
     </div>
   )
 }
